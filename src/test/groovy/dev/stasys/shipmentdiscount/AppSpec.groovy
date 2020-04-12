f10164a24ba2c9bc3bf38da7671eb613bbccd067
Stasys Skliutas <stasys.skliutas@gmail.com>
2020-04-12 09:30:18 -0700

    Initialize project

---
+++ src/test/groovy/dev/stasys/shipmentdiscount/AppSpec.groovy
@@ -0,0 +1,12 @@
+package dev.stasys.shipmentdiscount
+
+class AppSpec extends spock.lang.Specification {
+    def 'test app'() {
+        given:
+            true
+        when:
+            true
+        then:
+            true
+    }
+}
